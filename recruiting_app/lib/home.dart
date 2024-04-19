fcef2ef766fce8cd8867bcbee098a11fb2758a46
AFEEMAK <55965963+AFEEMAK@users.noreply.github.com>
2024-04-18 20:11:36 -0400

    populated home page with posts

--- recruiting_app/lib/home.dart
+++ recruiting_app/lib/home.dart
@@ -1,13 +1,12 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:recruiting_app/post.dart';
-import 'package:recruiting_app/candidate.dart';
-import 'package:recruiting_app/job_listing.dart';
-import 'package:recruiting_app/create_post.dart';
+import 'package:flutter/services.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:recruiting_app/sign_in.dart';
+import 'package:recruiting_app/post.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
@@ -19,9 +18,21 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     super.initState();
-    postList.add(Post("User1", "12/12/23", "Title 1", "This is the Description of Post 1"));
-    postList.add(Post("User2", "10/01/24", "Title 2", "This is the Description of Post 2"));
-    postList.add(Post("User2", "10/01/24", "Title 2", "This is the Description of Post 2"));
+    _loadPostData();
+  }
+
+  Future<void> _loadPostData() async {
+    try {
+      String jsonData = await rootBundle.loadString('assets/posts.json');
+      List<dynamic> jsonList = jsonDecode(jsonData);
+      List<Post> loadedPosts = jsonList.map((json) => Post.fromJson(json)).toList();
+
+      setState(() {
+        postList = loadedPosts;
+      });
+    } catch (e) {
+      print('Error loading post data: $e');
+    }
   }
 
   @override
@@ -38,9 +49,7 @@ class _HomeScreenState extends State<HomeScreen> {
           Padding(
             padding: const EdgeInsets.only(right: 20.0),
             child: ElevatedButton(
-            onPressed: () => _logout(),
-
-
+              onPressed: _logout,
               style: ButtonStyle(
                 backgroundColor: MaterialStateProperty.all(const Color(0xFFEEBBC3)),
                 padding: MaterialStateProperty.all(const EdgeInsets.fromLTRB(30, 15, 30, 15)),
@@ -57,59 +66,7 @@ class _HomeScreenState extends State<HomeScreen> {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           const SizedBox(height: 20),
-          SizedBox(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-
-                IconButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const CreatePostScreen()),
-                    );
-                  },
-                  icon: const Icon(Icons.add, size: 30, color: Color(0xFF232946)),
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(const Color(0xFFB8C1EC)),
-                    padding: MaterialStateProperty.all(const EdgeInsets.fromLTRB(4, 4, 4, 4)),
-                  ),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const CandidateScreen()),
-                    );
-                  },
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(const Color(0xFFB8C1EC)),
-                    padding: MaterialStateProperty.all(const EdgeInsets.fromLTRB(30, 15, 30, 15)),
-                  ),
-                  child: const Text(
-                    'VIEW USERS',
-                    style: TextStyle(fontSize: 16, color: Color(0xFF232946)),
-                  ),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const JobScreen()),
-                    );
-                  },
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(const Color(0xFFB8C1EC)),
-                    padding: MaterialStateProperty.all(const EdgeInsets.fromLTRB(30, 15, 30, 15)),
-                  ),
-                  child: const Text(
-                    'VIEW JOBS',
-                    style: TextStyle(fontSize: 16, color: Color(0xFF232946)),
-                  ),
-                ),
-              ],
-            ),
-          ),
+          // Your buttons row here
           const SizedBox(height: 10),
           Expanded(
             child: ListView.builder(
@@ -118,9 +75,7 @@ class _HomeScreenState extends State<HomeScreen> {
               itemBuilder: (context, index) {
                 return Padding(
                   padding: const EdgeInsets.symmetric(vertical: 5),
-
                   child: Container(
-
                     decoration: BoxDecoration(
                       color: const Color(0xFF232946),
                       borderRadius: BorderRadius.circular(5),
@@ -171,11 +126,12 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
     );
   }
+
   void _logout() async {
     try {
       await FirebaseAuth.instance.signOut();
       // Navigate to login screen or any other screen after logout
-      Navigator.push(
+      Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) => SignInScreen()),
       );
--- recruiting_app/lib/post.dart
+++ recruiting_app/lib/post.dart
@@ -1,9 +1,28 @@
-class Post
-{
+class Post {
   String user;
   String time;
   String title;
   String desc;
 
-  Post(this.user, this.time,this.title,this.desc);
+  Post(this.user, this.time, this.title, this.desc);
+
+  // Convert post object to JSON
+  Map<String, dynamic> toJson() {
+    return {
+      'user': user,
+      'time': time,
+      'title': title,
+      'desc': desc,
+    };
+  }
+
+  // Create post object from JSON
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      json['user'],
+      json['time'],
+      json['title'],
+      json['desc'],
+    );
+  }
 }
